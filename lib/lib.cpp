a26d188209f4b957217dc2642e81aa86e6759958
Tyler Gross <tylergross28@gmail.com>
2025-10-01 16:41:07 -0400

    promise version

--- lib/lib.cpp
+++ lib/lib.cpp
@@ -5,6 +5,7 @@
 #include <emscripten/bind.h>
 #include <emscripten/emscripten.h>
 #include <emscripten/threading.h>
+#include <future>
 
 std::string get_value(emscripten::val storage, const std::string& key) {
   auto result = storage.call<emscripten::val>("getItem", key);
@@ -17,36 +18,39 @@ std::string get_value(emscripten::val storage, const std::string& key) {
 
 struct SharedContext {
   std::string key;
-  std::function<void(std::string)> callback;
+  std::promise<std::string> promise;
 };
 
-void get_item_from_session_storage(const std::string& key, std::function<void(std::string)> callback) {
+std::string get_item_from_session_storage(const std::string& key) {
+  std::promise<std::string> promise;
+  std::future<std::string> future = promise.get_future();
+
   auto session_storage = emscripten::val::global("sessionStorage");
   if (session_storage != emscripten::val::undefined()) {
-    callback(get_value(session_storage, key));
-    return;
+    promise.set_value(get_value(session_storage, key));
+    return future.get();
   }
 
-  SharedContext* ctx = new SharedContext{std::move(key), std::move(callback)};
+  SharedContext* ctx = new SharedContext{std::move(key), std::move(promise)};
   emscripten_async_run_in_main_runtime_thread(EM_FUNC_SIG_VI, static_cast<void(*)(SharedContext*)>([] (auto raw_ctx) {
     auto session_storage = emscripten::val::global("sessionStorage");
-    raw_ctx->callback(get_value(session_storage, raw_ctx->key));
+    raw_ctx->promise.set_value(get_value(session_storage, raw_ctx->key));
 
     delete raw_ctx;
   }), ctx);
+
+  return future.get();
 }
 
 void get_from_main_thread() {
-  get_item_from_session_storage("name", [] (auto value) {
-    printf("from main thread: %s\n", value.c_str());
-  });
+  auto val = get_item_from_session_storage("name");
+  printf("from main thread: %s\n", val.c_str());
 }
 
 void get_from_thread() {
   std::thread([] {
-    get_item_from_session_storage("name", [] (auto value) {
-      printf("from other thread: %s\n", value.c_str());
-    });
+    auto val = get_item_from_session_storage("name");
+    printf("from other thread: %s\n", val.c_str());
   }).detach();
 }
 
